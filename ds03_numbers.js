168b1228c2080f70a1aa30129dbecc9cfdde4acc
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-21 17:55:00 +0800

    numbers

--- ds03_numbers.js
+++ ds03_numbers.js
@@ -2,47 +2,60 @@
 0.1 + 0.2 === 0.3;
 
 // -------------------------------------------------------------Integer Rounding
-5/4; // 1.25
-
-Math.floor(0.9); // 0
-Math.floor(1.1); // 1
-
-Math.round(0.49); // 0
-Math.round(0.5); // 1
-Math.round(2.9); // 3
-
-Math.ceil(0.1); // 1
-Math.ceil(0.9); // 1
-Math.ceil(21); // 21
-Math.ceil(21.01); // 22
+5 / 4;
+// 1.25
+Math.floor(0.9);
+// 0
+Math.floor(1.1);
+// 1
+Math.round(0.49);
+// 0
+Math.round(0.5);
+// 1
+Math.round(2.9);
+// 3
+Math.ceil(0.1);
+// 1
+Math.ceil(0.9);
+// 1
+Math.ceil(21);
+// 21
+Math.ceil(21.01);
+// 22
 
 // ---------------------------------------------------------------Number.EPSILON
 function numberEquals(x, y) {
     return Math.abs(x - y) < Number.EPSILON;
 }
-0.1 + 0.2 == 0.3 // false due to little difference in floating point
-numberEquals(0.1 + 0.2, 0.3); // true
+
+0.1 + 0.2 == 0.3;
+// false due to little difference in floating point
+numberEquals(0.1 + 0.2, 0.3);
+// true
 
 // ---------------------------------------------------------------------Maximums
-Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2; // true
-
-Number.MAX_SAFE_INTEGER + 1.111 === Number.MAX_SAFE_INTEGER + 2.022; // false
-
-Number.MAX_VALUE + 1 === Number.MAX_VALUE + 2; // true
-
-Number.MAX_VALUE + 1.111 === Number.MAX_VALUE + 2.022; // true
+Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2;
+// true
+Number.MAX_SAFE_INTEGER + 1.111 === Number.MAX_SAFE_INTEGER + 2.022;
+// false
+Number.MAX_VALUE + 1 === Number.MAX_VALUE + 2;
+// true
+Number.MAX_VALUE + 1.111 === Number.MAX_VALUE + 2.022;
+// true
 
 // ---------------------------------------------------------------------Minimums
-Number.MIN_SAFE_INTEGER - 1 === Number.MIN_SAFE_INTEGER - 2; // true
-
-Number.MIN_SAFE_INTEGER - 1.111 === Number.MIN_SAFE_INTEGER - 2.022; // false
-
-Number.MIN_VALUE - 1 == -1; // true
+Number.MIN_SAFE_INTEGER - 1 === Number.MIN_SAFE_INTEGER - 2;
+// true
+Number.MIN_SAFE_INTEGER - 1.111 === Number.MIN_SAFE_INTEGER - 2.022;
+// false
+Number.MIN_VALUE - 1 == -1;
+// true
 
 // ---------------------------------------------------------------------Infinity
-Infinity > Number.MAX_SAFE_INTEGER; // true
--Infinity < Number.MAX_SAFE_INTEGER // true
--Infinity -32323323 == -Infinity -1; // true
+Infinity > Number.MAX_SAFE_INTEGER;
+// true
+-Infinity < Number.MAX_SAFE_INTEGER - Infinity - 32323323 == -Infinity - 1;
+// true
 
 // ---------------------------------------------------------------Primality Test
 function isPrime(n) {
@@ -57,6 +70,7 @@ function isPrime(n) {
     }
     return true;
 }
+
 isPrime(31);
 
 function isPrime(n) {
@@ -66,11 +80,11 @@ function isPrime(n) {
     // middle five numbers in below loop
     if (n % 2 == 0 || n % 3 == 0) return false;
     for (var i = 5; i * i <= n; i = i + 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
+        if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
 }
+
 isPrime(31);
 
 // ----------------------------------------------------------Prime Factorization
@@ -94,33 +108,41 @@ function primeFactors(n) {
         console.log(n);
     }
 }
-primeFactors(10); // prints '5' and '2'
 
-Math.random() * 100; // floats between 0 and 100
-Math.random() * 25 + 5; // floats between 5 and 30
-Math.random() * 10 - 100; // floats between -100 and -90
-
-Math.floor(Math.random()) * 100; // integer between 0 and 99
-Math.round(Math.random()) * 25 + 5; // integer between 5 and 30
-Math.ceil(Math.random()) * 10 - 100; // integer between -100 and -90
+primeFactors(10);
+// prints '5' and '2'
+Math.random() * 100;
+// floats between 0 and 100
+Math.random() * 25 + 5;
+// floats between 5 and 30
+Math.random() * 10 - 100;
+// floats between -100 and -90
+Math.floor(Math.random()) * 100;
+// integer between 0 and 99
+Math.round(Math.random()) * 25 + 5;
+// integer between 5 and 30
+Math.ceil(Math.random()) * 10 - 100;
+// integer between -100 and -90
 
 // -------------------------------------------------------modular exponentiation
 function modularExponentiation(base, exponent, modulus) {
     return Math.pow(base, exponent) % modulus;
 }
-modularExponentiation(4,3,5); // 4
+
+modularExponentiation(4, 3, 5);
+// 4
 
 function modularExponentiation(base, exponent, modulus) {
     if (modulus == 1) return 0;
-
     var value = 1;
-
     for (var i = 0; i < exponent; i++) {
         value = (value * base) % modulus;
     }
     return value;
 }
-modularExponentiation(4,3,5); // 4
+
+modularExponentiation(4, 3, 5);
+// 4
 
 // -------------------------------------------------Print all primes less than n
 function allPrimesLessThanN(n) {
@@ -162,4 +184,6 @@ function arrayNUglyNumbers(n) {
     }
     return uglyNumbers;
 }
-arrayNUglyNumbers(12); // [ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16 ]
+
+arrayNUglyNumbers(12);
+// [ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16 ]
