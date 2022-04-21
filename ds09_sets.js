3267d46e7ed8ea9252246be770faf8f35fb18467
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-21 18:24:41 +0800

    sets

--- ds09_sets.js
+++ ds09_sets.js
@@ -1,26 +1,37 @@
 // --------------------------------------------------------------------Insertion
 var exampleSet = new Set();
-exampleSet.add(1); // exampleSet: Set(1) {1}
-exampleSet.add(1); // exampleSet: Set(1) {1}
-exampleSet.add(2); // exampleSet: Set(2) {1, 2}
-exampleSet
+exampleSet.add(1);
+// exampleSet: Set(1) {1}
+exampleSet.add(1);
+// exampleSet: Set(1) {1}
+exampleSet.add(2);
+// exampleSet: Set(2) {1, 2}
+exampleSet;
 
 // ---------------------------------------------------------------------Deletion
 var exampleSet = new Set();
-exampleSet.add(1); // exampleSet: Set {1}
-exampleSet.delete(1); // true
-exampleSet.add(2); // exampleSet: Set {1, 2}
+exampleSet.add(1);
+// exampleSet: Set {1}
+exampleSet.delete(1);
+// true
+exampleSet.add(2);
+// exampleSet: Set {1, 2}
 
 // ---------------------------------------------------------------------Contains
 var exampleSet = new Set();
-exampleSet.add(1); // exampleSet: Set(1) {1}
-exampleSet.has(1); // true
-exampleSet.has(2); // false
-exampleSet.add(2); // exampleSet: Set(2) {1, 2}
-exampleSet.has(2); // true
+exampleSet.add(1);
+// exampleSet: Set(1) {1}
+exampleSet.has(1);
+// true
+exampleSet.has(2);
+// false
+exampleSet.add(2);
+// exampleSet: Set(2) {1, 2}
+exampleSet.has(2);
+// true
 
 // -----------------------------------------------------------------Intersection
-function intersectSets (setA, setB) {
+function intersectSets(setA, setB) {
     var intersection = new Set();
     for (var elem of setB) {
         if (setA.has(elem)) {
@@ -32,10 +43,11 @@ function intersectSets (setA, setB) {
 
 var setA = new Set([1, 2, 3, 4]),
     setB = new Set([2, 3]);
-intersectSets(setA, setB); // Set {2, 3}
+intersectSets(setA, setB);
+// Set {2, 3}
 
 // -------------------------------------------------------------------isSuperSet
-function isSuperset (setA, subset) {
+function isSuperset(setA, subset) {
     for (var elem of subset) {
         if (!setA.has(elem)) {
             return false;
@@ -47,7 +59,7 @@ function isSuperset (setA, subset) {
 var setA = new Set([1, 2, 3, 4]),
     setB = new Set([2, 3]),
     setC = new Set([5]);
-isSuperset(setA, setB); 
+isSuperset(setA, setB);
 
 setA.isSuperset(setC);
 
@@ -78,7 +90,8 @@ function differenceSet(setA, setB) {
 
 var setA = new Set([1, 2, 3, 4]),
     setB = new Set([2, 3]);
-differenceSet(setA, setB); // Set(2) {1, 4}
+differenceSet(setA, setB);
+// Set(2) {1, 4}
 
 // -------------------Exercises 1 USING SETS TO CHECK FOR DUPLICATES IN AN ARRAY
 function checkDuplicates(arr) {
@@ -86,9 +99,10 @@ function checkDuplicates(arr) {
     return mySet.size < arr.length;
 }
 
-checkDuplicates([1,2,3,4,5]); // false
-checkDuplicates([1,1,2,3,4,5]); // true
-
+checkDuplicates([1, 2, 3, 4, 5]);
+// false
+checkDuplicates([1, 1, 2, 3, 4, 5]);
+// true
 
 // -----------------Exercises 2 RETURNING ALL UNIQUE VALUES FROM SEPARATE ARRAYS
 function uniqueList(arr1, arr2) {
@@ -96,6 +110,9 @@ function uniqueList(arr1, arr2) {
     return Array.from(mySet);
 }
 
-uniqueList([1,1,2,2],[2,3,4,5]); // [1,2,3,4,5]
-uniqueList([1,2],[3,4,5]); // [1,2,3,4,5]
-uniqueList([],[2,2,3,4,5]); // [2,3,4,5]
+uniqueList([1, 1, 2, 2], [2, 3, 4, 5]);
+// [1,2,3,4,5]
+uniqueList([1, 2], [3, 4, 5]);
+// [1,2,3,4,5]
+uniqueList([], [2, 2, 3, 4, 5]);
+// [2,3,4,5]
